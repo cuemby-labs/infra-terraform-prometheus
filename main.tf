372a6fdf18e6ba470e1fbe8ed1afd5b7e618566c
Lewisjmp <lewis.munoz@cuemby.com>
2024-10-03 16:47:34 -0500

    Update Values to Prometheus and AlertManager

--- main.tf
+++ main.tf
@@ -16,16 +16,7 @@ resource "helm_release" "kube-prometheus-alert" {
   chart      = "kube-prometheus-stack"
   version    = var.chart_version
 
-  # Ingress values
-  values = [
-    templatefile("${path.module}/values.yaml.tpl", {
-      domain_name           = var.domain_name,
-      dash_domain_name      = var.dash_domain_name
-      issuer_name           = var.issuer_name
-      issuer_kind           = var.issuer_kind
-      channel               = var.channel
-    })
-  ]
+  values     = [file("${path.module}/values.yaml")]
 }
 
 #
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 #
-# Prometheus variables
+# Harbor variables
 #
 
 variable "release_name" {
@@ -15,44 +15,9 @@ variable "namespace_name" {
 }
 
 variable "chart_version" {
-  description = "The version of the ingress-nginx Helm chart."
+  description = "The version of the Prometheus Helm chart."
   type        = string
   default     = "64.0.0"
-}
-
-#
-# Prometheus manifest variables
-#
-
-variable "domain_name" {
-  type        = string
-  description = "domain name for Prometheus, e.g. 'dev.domainname.com'"
-  default     = "dev.domainname.com"
-}
-
-variable "dash_domain_name" {
-  type        = string
-  description = "domain name with dash, e.g. 'dev-domainname-com'"
-  default     = "dev-domainname-com"
-}
-
-variable "issuer_name" {
-  type        = string
-  description = "origin issuer name"
-  default     = "origin-ca-issuer"
-}
-
-variable "issuer_kind" {
-  type        = string
-  description = "origin issuer kind"
-  default     = "ClusterOriginIssuer"
-}
-
-#View Documentation to Ms Teams.
-variable "channel" {
-  type        = string
-  description = "Channel config to Microsoft Teams"
-  default     = "alerts"
 }
 
 #
